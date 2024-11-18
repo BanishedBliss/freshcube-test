13a0680a12ac776a27001fc40331a33b192f60f7
Валерий Т. <mythical.form.of.life@gmail.com>
2024-11-18 19:47:33 +0800

    - Опубликованы файлы пагинации и локализации. - Добавлена русская локализация.

---
+++ lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Ваш пароль был сброшен.',
+    'sent' => 'Мы отправили Вам ссылку на восстановление пароля.',
+    'throttled' => 'Пожалуйста, подождите, перед тем как повторить попытку.',
+    'token' => 'Ссылка на восстановление пароля не верна.',
+    'user' => "Мы не можем найти пользователя с введённым email'ом.",
+
+];
